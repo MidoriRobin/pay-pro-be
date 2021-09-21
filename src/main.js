8f68099423f81ece4e18937ac1ffe3ca641642b5
Christopher Robinson <chrispaul_robinson@hotmail.com>
2021-09-20 20:32:41 -0500

    adjust main for env variables

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // await app.listen(process.env.REACT_APP_PORT);
-  await app.listen(8080);
+  await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
